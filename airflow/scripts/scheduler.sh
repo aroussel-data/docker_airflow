dd3713962d2822f40545fd9fa84cfb7fe523a0d0
alexandredrr14 <alexandredrr14@gmail.com>
2021-01-24 21:53:53 +0100

    typo

--- airflow/scripts/scheduler.sh
+++ airflow/scripts/scheduler.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# To enable this script to activate the virtualenv and set AIRFLOW_HOME, it must be run from FenixDataScience
+# To enable this script to activate the virtualenv and set AIRFLOW_HOME, it must be run from working dir
 cd $(dirname "$0")/../..
 
 source venv/bin/activate
